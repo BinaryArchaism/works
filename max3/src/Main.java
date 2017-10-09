68bb9fc796ff2243b8f4fcddc147db96002423da
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-10-09 19:42:49 +0300

    v1

---
+++ max3/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Dmitrii on 09.10.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {10, 5, 7, 23, 5, 3, 65, 4, 12, 3};
+        int max1, max2, max3;
+        if (a[0] > a[1]) {
+            
+        }
+    }
+}
