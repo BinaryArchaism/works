b32066946e7001019478a6e98aa88b24b930dd68
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2018-02-07 21:21:11 +0300

    addSum

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -5,12 +5,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(recursion(7, 1));
+        System.out.println(sum(8, 16));
     }
 
-    public static int recursion(int n, int s) {
+    public static int recursion1(int n, int s) {
         if (n == 0) return s;
         else s = n * s;
-        return recursion(n-1, s);
+        return recursion1(n-1, s);
+    }
+
+    public static int sum(int n, int m) {
+        if (n == 0) return m;
+        return sum(--n, ++m);
     }
 }
